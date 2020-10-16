d3b21c941840dde16bc46aca3293a41543c5d420
Raul Alonso <raul@raul>
2020-10-16 12:13:54 +0200

    subida primera parte

---
+++ src/main/java/com/raul/ej4/Aleatorio.java
@@ -0,0 +1,51 @@
+package com.raul.ej4;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Aleatorio
+ */
+@WebServlet("/numero")
+public class Aleatorio extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+	
+    public Aleatorio() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		out.print("<html> <body>");
+		
+		out.print("<h1> El número generado es: " + Math.random()*25+1);
+
+		out.print("</h1> </body> </html>");
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h2>pulsa el boton para generar un Número aleatorio</h2>
+<form action="http://localhost:8080/Ejercicio4/numero" method="post">
+	<input type="submit" value="Enviar">
+</form>
+</body>
+</html>
